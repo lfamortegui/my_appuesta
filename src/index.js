5692b8818dd3ed98d985de94906f7f20800a702a
lfamortegui <lfamortegui@uninorte.edu.co>
2021-11-28 00:06:43 -0500

    Commit inicial

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import App2 from './App2';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>} />
+        <Route path="/index" element={<App2/>} />
+      </Routes>
+    </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
